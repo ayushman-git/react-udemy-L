f84b90932e0c5406a2db6a14d2195a73d3c3bd60
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-01-16 15:18:19 +0530

    using createElement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-     <p>Hello</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "App"
+      },
+      React.createElement("h2", null, "Using createElement")
+    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hello</h1>
+    //   </div>
+    // );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1>Hello</h1>
+//     </div>
+//   );
+// }
 
 export default App;
