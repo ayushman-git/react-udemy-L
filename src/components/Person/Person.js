0a71e6afea511436845aae3d23bb9e16a5e3fc55
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-01-17 17:41:04 +0530

    using useState

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,10 +1,20 @@
-const person = (props) => {
+import { useState } from "react";
+
+const Person = (props) => {
+  const [ageState, setAgeState] = useState({
+    age: 30,
+  });
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
   return (
     <div>
       <strong>Random {props.title}:</strong> {Math.floor(Math.random() * 100)}
+      <p>Age: {ageState.age}</p>
+      <button onClick={clickHandler}>Click</button>
       <p>{props.children}</p>
     </div>
   );
 };
 
-export default person;
+export default Person;
