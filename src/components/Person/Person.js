acff9a4d08ea8ba9713e67be9483b054ae8ec591
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-01-20 10:43:52 +0530

    scoped pseudo classes usin radium

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Person.css";
+import Radium from "radium";
 
 const Person = (props) => {
   const [ageState, setAgeState] = useState({
@@ -13,19 +14,25 @@ const Person = (props) => {
   };
 
   const buttonStyle = {
-    borderRadius: "20px",
+    borderRadius: "7px",
     padding: "1em",
     backgroundColor: "black",
     color: "white",
     outline: "none",
     border: "none",
+    transition: "all 0.2s ease",
+    ":hover": {
+      backgroundColor: "white",
+      color: "black",
+      borderRadius: "10px",
+    },
   };
 
   return (
     <div className="Person">
       <strong onClick={props.click}>Random {props.title}:</strong>{" "}
       {Math.floor(Math.random() * 100)}
-      <p>Age: {ageState.age}</p>
+      <p className="Person-strong">Age: {ageState.age}</p>
       <input type="text" onChange={props.change} value={props.title} />
       <button style={buttonStyle} onClick={clickHandler}>
         Click
@@ -35,4 +42,4 @@ const Person = (props) => {
   );
 };
 
-export default Person;
+export default Radium(Person);
